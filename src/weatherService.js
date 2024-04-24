87f496048cb584cd2e55565301cf4e1e3ab6419a
ShampaShaw <shampashaw088@gmail.com>
2024-04-24 10:51:10 +0530

    Update API key variable name in weatherService.js

--- src/weatherService.js
+++ src/weatherService.js
@@ -1,9 +1,9 @@
-const API_KEY = '21287ce169a6a9290f46e9e5de0ee13b';
+const apiKey = process.env.REACT_APP_API_KEY;
 
 const makeIconURL = (iconId) => `http://openweathermap.org/img/wn/${iconId}@2x.png`;
 
 const getFormattedWeatherData = async (city, cnt = 14, units = 'metric') => {
-  const URL = `https:/api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${units}`;
+  const URL = `https:/api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 
   const data = await fetch(URL)
     .then((res) => res.json())
